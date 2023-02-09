a27f0f9db9750f0c5876d520cd3338d6f8016587
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-09 20:57:43 -0300

    feat: add vite project template

--- src/main.js
+++ src/main.js
@@ -1,27 +1,15 @@
-import "primevue/resources/themes/lara-light-indigo/theme.css";
-import "primevue/resources/themes/saga-blue/theme.css";
-import "primevue/resources/primevue.min.css";
-import "primeicons/primeicons.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-import Toast from "primevue/toast";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
-import router from "./router/index.js";
-import TabMenu from "primevue/tabmenu";
 import "/node_modules/primeflex/primeflex.css";
-// import ToastService from "primevue/toastservice";
 
 const app = createApp(App);
-app.use(router);
 app.use(PrimeVue);
-// app.use(ToastService);
-
-app.component("PrimeButton", Button);
-app.component("PrimeInputText", InputText);
-app.component("PrimeToast", Toast);
-app.component("PrimeTabMenu", TabMenu);
+app.component("Button", Button);
 
 app.mount("#app");
