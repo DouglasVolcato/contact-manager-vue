7893112164df61f6c15ad7eb6d5ba4054f3b5d7e
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-10 00:05:09 -0300

    feat: add plus button

--- src/view/ContactManager.vue
+++ src/view/ContactManager.vue
@@ -1,5 +1,10 @@
 <template>
-  <Title text="Contact Manager" />
+  <div class="flex flex-row flex-wrap justify-content-center">
+    <Title text="Contact Manager" />
+    <Button class="p-button-success m-2 mt-3"
+      >Add<i class="pi pi-plus-circle text-xl"></i
+    ></Button>
+  </div>
 </template>
 
 <script>
