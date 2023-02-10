4f861405bd17392b80e04f9d3ef49781f51dc8ad
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-10 00:12:39 -0300

    feat: add search components

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import TabMenu from "primevue/tabmenu";
+import InputText from "primevue/inputtext";
 
 import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -15,6 +16,7 @@ const app = createApp(App);
 app.use(router);
 // app.use(store);
 app.use(PrimeVue);
+app.component("InputText", InputText);
 app.component("TabMenu", TabMenu);
 app.component("Button", Button);
 app.mount("#app");
--- src/view/ContactManager.vue
+++ src/view/ContactManager.vue
@@ -5,6 +5,10 @@
       >Add<i class="pi pi-plus-circle text-xl"></i
     ></Button>
   </div>
+  <form class="flex flex-row flex-wrap justify-content-center mt-5">
+    <Button class="mr-2"> <i class="pi pi-search mt-1 mb-1"></i></Button>
+    <InputText placeholder="Search" />
+  </form>
 </template>
 
 <script>
