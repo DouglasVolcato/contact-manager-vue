e28af4c74dcf9fc144b7d5fa204e25f0286ed5fa
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-10 00:13:55 -0300

    feat: implement search components

--- src/view/ContactManager.vue
+++ src/view/ContactManager.vue
@@ -6,8 +6,8 @@
     ></Button>
   </div>
   <form class="flex flex-row flex-wrap justify-content-center mt-5">
-    <Button class="mr-2"> <i class="pi pi-search mt-1 mb-1"></i></Button>
     <InputText placeholder="Search name" />
+    <Button class="ml-2"> <i class="pi pi-search mt-1 mb-1"></i></Button>
   </form>
 </template>
 
