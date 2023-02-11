93490a033bf9584ebbf94c157675f271c3391dc2
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-11 16:13:24 -0300

    refactor: delete unused menus

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,24 +16,9 @@ export default {
       items: [
         { label: "Home", icon: "pi pi-fw pi-home", to: "/" },
         {
-          label: "ContactManager",
-          icon: "pi pi-fw pi-calendar",
-          to: "/contacts",
-        },
-        {
           label: "AddContact",
-          icon: "pi pi-fw pi-pencil",
+          icon: "pi pi-plus-circle text-xl",
           to: "/contacts/add",
-        },
-        {
-          label: "EditContact",
-          icon: "pi pi-fw pi-file",
-          to: "/contacts/edit/:contactId",
-        },
-        {
-          label: "ViewContact",
-          icon: "pi pi-fw pi-cog",
-          to: "/contacts/view/:contactId",
         },
       ],
     };
