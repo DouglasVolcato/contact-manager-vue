e00120046bd4e0413f66bc88c5febc5af0562ffc
Douglas Volcato <douglasvolcato@gmail.com>
2023-02-11 22:26:39 -0300

    chore: add mocked data

---
+++ src/api/mocks.js
@@ -0,0 +1,33 @@
+const contacts = [
+  {
+    id: "2rf324fx43q24rx34134f",
+    name: "Douglas",
+    photoUrl: "https://avatars.githubusercontent.com/u/106124397?v=4",
+    email: "doug@gmail.com",
+    mobile: "230422342",
+    company: "DB-Seller",
+    title: "Fullstack Developer",
+  },
+  {
+    id: "xurm02m9mrx0923r800ff",
+    name: "Lucas",
+    photoUrl:
+      "https://images.pexels.com/photos/62289/yemen-chameleon-chamaeleo-calyptratus-chameleon-reptile-62289.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    email: "lucas@gmail.com",
+    mobile: "4534534535",
+    company: "Anonymous",
+    title: "Frontend Developer",
+  },
+  {
+    id: "923mj309imzdjmefjklf9",
+    name: "Maria",
+    photoUrl:
+      "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    email: "maria@gmail.com",
+    mobile: "555555555",
+    company: "Microsoft",
+    title: "Backend Developer",
+  },
+];
+
+export default contacts;
